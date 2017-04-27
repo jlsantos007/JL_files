853103f0f3bec342b8073ff90409fa2e86b1799c
ngkirito <jlsantos007.lebie@gmail.com>
2017-04-27 12:11:50 +0800

    sample web exercises

---
+++ application/views/session_view.php
@@ -0,0 +1,13 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>CodeIgniter Session Example</title>
+    </head>
+
+    <body>
+        Welcome <?php echo $this->session->userdata('name');?>
+        <br>
+        <a href="http://localhost/test/index.php/sessionex/unset_session_data">Click Here</a> to unset session data.
+    </body>
+
+</html>
